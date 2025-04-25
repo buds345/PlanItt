132330299402209641e239483dd547ab960e6d58
banelexhamlashe345 <banelebanele938@gmail.com>
2025-04-25 13:34:13 +0200

    Update email_utils.py

--- auth/__pycache__/email_utils.py
+++ auth/__pycache__/email_utils.py
@@ -1,1 +1,14 @@
+import smtplib
+from email.message import EmailMessage
+
+def send_verification_email(to_email, verification_link):
+    msg = EmailMessage()
+    msg["Subject"] = "Verify your email address"
+    msg["From"] = "your-email@gmail.com"  # replace with your email
+    msg["To"] = to_email
+    msg.set_content(f"Click this link to verify your email: {verification_link}")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login("banelebanele938@gmail.com", "aqchitvwzzymkxvo")  # replace these
+        smtp.send_message(msg)
 
